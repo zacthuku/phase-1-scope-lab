f1b9446b21b0d07586a5fd11c255105a7a2037a8
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-07 00:18:55 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName='bob'
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer='bob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='cane'
+}
